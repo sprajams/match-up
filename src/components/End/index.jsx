9a5fa0379c3e8f699ef0d65e010f7246630b4427
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-01 18:47:58 -0700

    end screen, handle replay option

--- src/components/End/index.jsx
+++ src/components/End/index.jsx
@@ -1,11 +1,49 @@
+import clsx from "clsx";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
-const End = ({ onClick }) => {
+const End = ({ restart, pickDifficulty }) => {
+  const [selectionActive, setSelectionActive] = useState(false);
+  const onClick = () => {
+    setSelectionActive(!selectionActive);
+  };
   return (
-    <div>
-      <button onClick={onClick} className={styles.replayBtn}>
-        Play again
-      </button>
+    <div className={styles.outer}>
+      {selectionActive ? (
+        <div>
+          <h3 onClick={onClick}>Select level of difficulty:</h3>
+          <div className={styles.levelWrap}>
+            <button
+              onClick={pickDifficulty}
+              value={4}
+              className={clsx(styles.btn, styles.diffultyBtn)}
+            >
+              Easy
+            </button>
+            <button
+              onClick={pickDifficulty}
+              value={6}
+              className={clsx(styles.btn, styles.diffultyBtn)}
+            >
+              Medium
+            </button>
+            <button
+              onClick={pickDifficulty}
+              value={8}
+              className={clsx(styles.btn, styles.diffultyBtn)}
+            >
+              Hard
+            </button>
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={onClick}
+          className={clsx(styles.btn, styles.replayBtn)}
+        >
+          Play again
+        </button>
+      )}
     </div>
   );
 };
